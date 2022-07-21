2556ddf37dfb4efaad87246c397fcfec9826340b
yuki_kawano <tetsuwan30@users.noreply.github.com>
2022-07-22 00:23:25 +0900

    Update 04_select_scores_join_members.sql

--- sample_scores/04_select_scores_join_members.sql
+++ sample_scores/04_select_scores_join_members.sql
@@ -2,8 +2,8 @@
 SELECT
   teams.pos AS team_pos
   ,teams.team_name
-  , members.id AS member_id
-  , members.member_name
+  ,members.id AS member_id
+  ,members.member_name
 FROM
   sample_scores.teams teams
   INNER JOIN sample_scores.members members
