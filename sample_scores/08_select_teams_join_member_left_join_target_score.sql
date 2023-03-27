131d71795240df24c04a4f28bb11b42bbfd7e140
tetsuwan30 <kawano.yuki@gmail.com>
2023-03-28 00:46:56 +0900

    インデントの調整

--- sample_scores/08_select_teams_join_member_left_join_target_score.sql
+++ sample_scores/08_select_teams_join_member_left_join_target_score.sql
@@ -9,14 +9,13 @@ select
 from
   sample_scores.teams teams
   inner join sample_scores.members members on teams.id = members.team_id
-  left join
-    (
-      select
-        scores.member_id
-        ,scores.match_id
-        ,scores.score
-      from
-        sample_scores.scores scores
-        inner join sample_scores.matches matches on matches.id = scores.match_id
-      where matches.match_on between '2022-06-01' and '2022-06-30'
-    ) target_score on target_score.member_id = members.id
+  left join (
+    select
+      scores.member_id,
+      scores.match_id,
+      scores.score
+    from
+      sample_scores.scores scores
+      inner join sample_scores.matches matches on matches.id = scores.match_id
+    where matches.match_on between '2022-06-01' and '2022-06-30'
+  ) target_score on target_score.member_id = members.id
--- sample_scores/09_select_teams_join_members_join_target_score_group_by_teams.sql
+++ sample_scores/09_select_teams_join_members_join_target_score_group_by_teams.sql
@@ -6,22 +6,16 @@ select
   sum(target_score.score) as score_sum
 from
   sample_scores.teams teams
-    inner join sample_scores.members members on teams.id = members.team_id
-    left join
-    (
-      select
-        scores.member_id
-        ,scores.match_id
-        ,scores.score
-      from
-        sample_scores.scores scores
-          inner join sample_scores.matches matches on matches.id = scores.match_id
-      where matches.match_on between '2022-06-01' and '2022-06-30'
-    ) target_score
-     on target_score.member_id = members.id
-group by
-  teams.pos
-  ,teams.team_name
-order by
-  teams.pos
-;
+  inner join sample_scores.members members on teams.id = members.team_id
+  left join (
+    select
+      scores.member_id,
+      scores.match_id,
+      scores.score
+    from
+      sample_scores.scores scores
+      inner join sample_scores.matches matches on matches.id = scores.match_id
+    where matches.match_on between '2022-06-01' and '2022-06-30'
+  ) target_score on target_score.member_id = members.id
+group by teams.pos, teams.team_name
+order by teams.pos
